6afa8c5bf0ecff0706e9895e3626692b0093984d
JustinLn <102838453+Glorious-Toast@users.noreply.github.com>
2024-01-03 22:48:43 -0700

    Add Test Player

---
+++ Town Game/Assets/Scripts/OfflineMode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OfflineMode : MonoBehaviour
+{
+    private void Awake()
+    {
+        PhotonNetwork.OfflineMode = true;
+    }
+}
