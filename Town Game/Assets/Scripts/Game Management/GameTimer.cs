8e2e82423fd53ac6e3213305321ba92319bc8fe4
JustinLn <102838453+Glorious-Toast@users.noreply.github.com>
2024-02-11 22:55:01 -0700

    Add Game TImer + Waiting Room Manager

---
+++ Town Game/Assets/Scripts/Game Management/GameTimer.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.Events;
+using TMPro;
+
+public class GameTimer : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public bool isTicking = false;
+    public UnityEvent onTimerStart;
+    public UnityEvent onTimerFinish;
+    public UnityEvent onTimerStop;
+    public TextMeshProUGUI timerText;
+    public float gameTimer;
+    public float clientTimer;
+    PhotonView view;
+
+    public void StartTimer(float time)
+    {
+        gameTimer = time;
+        onTimerStart.Invoke();
+        isTicking = true;
+        clientTimer = gameTimer;
+    }
+
+    public void StopTimer()
+    {
+        onTimerStop.Invoke();
+        isTicking = false;
+    }
+
+    public void ShowTimerText()
+    {
+        timerText.gameObject.SetActive(true);
+    }
+
+    public void HideTimerText()
+    {
+        timerText.gameObject.SetActive(false);
+    }
+
+    void Awake()
+    {
+        view = gameObject.GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        ClientTimer();
+        TimerUI();
+        if (!PhotonNetwork.IsMasterClient) return;
+        Timer();
+    }
+
+    void TimerUI()
+    {
+        timerText.text = Mathf.Ceil(clientTimer).ToString();
+    }
+
+    void Timer()
+    {
+        if (!isTicking) return;
+        gameTimer -= Time.deltaTime;
+        clientTimer = gameTimer;
+        if (gameTimer < 0f)
+        {
+            FinishTimer();
+        }
+    }
+
+    void FinishTimer()
+    {
+        onTimerFinish.Invoke();
+        isTicking = false;
+    }
+
+    void ClientTimer()
+    {
+        if (PhotonNetwork.IsMasterClient) return;
+        if (!isTicking) return;
+
+        if (clientTimer > gameTimer)
+        {
+            clientTimer = gameTimer;
+        }
+        if (gameTimer - clientTimer > .5f)
+        {
+            clientTimer = gameTimer;
+        }
+
+        clientTimer -= Time.deltaTime;
+        if (clientTimer < 0f) clientTimer = -0.1f;
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(gameTimer);
+            stream.SendNext(isTicking);
+        }
+        else
+        {
+            gameTimer = (float)stream.ReceiveNext();
+            isTicking = (bool)stream.ReceiveNext();
+        }
+    }
+}
---
+++ Town Game/Assets/Scripts/Game Management/WaitingRoomManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class WaitingRoomManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+    // 0 = not ready 1 = ready 2 = starting
+    public int roomPhase = 0;
+    public int playersRequired = 2;
+    // Objects that correspond to a gamephase
+    public GameObject[] phaseUI;
+    public GameObject hostUI;
+    GameTimer gt;
+    int previousRoomPhase = 0;
+
+    void Awake()
+    {
+        gt = gameObject.GetComponent<GameTimer>();
+    }
+
+    void Update()
+    {
+        PhaseUI();
+        if (!PhotonNetwork.IsMasterClient) return;
+        PhaseLogic();
+    }
+
+    void PhaseLogic()
+    {
+        if (roomPhase == 0)
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount >= playersRequired)
+            {
+                roomPhase = 1;
+            }
+            if (PhotonNetwork.IsMasterClient) hostUI.SetActive(false);
+        }
+        if (roomPhase == 1)
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount < playersRequired)
+            {
+                roomPhase = 0;
+                return;
+            }
+            if (PhotonNetwork.IsMasterClient)
+            {
+                hostUI.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    roomPhase = 2;
+                    StartTimer();
+                    return;
+                }
+            }
+        }
+        if (roomPhase == 2)
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                hostUI.SetActive(false);
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    roomPhase = 1;
+                    gt.StopTimer();
+                }
+            }
+        }
+    }
+
+    void StartTimer()
+    {
+        Debug.Log("meow");
+        gt.StartTimer(5f);
+    }
+
+    void PhaseUI()
+    {
+        if (previousRoomPhase == roomPhase) return;
+        previousRoomPhase = roomPhase;
+
+        foreach (GameObject ui in phaseUI)
+        {
+            ui.SetActive(false);
+        }
+        phaseUI[roomPhase].SetActive(true);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) 
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(roomPhase);
+        } else
+        {
+            roomPhase = (int)stream.ReceiveNext();
+        }
+    }
+}   
