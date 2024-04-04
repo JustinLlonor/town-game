06ef595d8eef1fe7bb3d477d7a09e757ab32c0c0
JustinLn <102838453+Glorious-Toast@users.noreply.github.com>
2024-04-04 00:39:41 -0600

    Add Minimap Room Enter Text

---
+++ Town Game/Assets/Scripts/UI/MinimapRoomText.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MinimapRoomText : MonoBehaviour
+{
+    public PlayerRoom trackedPM;
+    TextMeshProUGUI text;
+    Animator animator;
+
+    private void Awake()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+        animator = gameObject.GetComponent<Animator>();
+        PlayerManager pm = FindObjectOfType<PlayerManager>();
+        pm.OnInstantiatePlayer += AddReferences;
+        if (trackedPM != null) trackedPM.OnEnterRoom += PlayRoomText;
+    }
+
+    void AddReferences(ref GameObject player)
+    {
+        trackedPM = player.GetComponent<PlayerRoom>();
+        trackedPM.OnEnterRoom += PlayRoomText;
+    }
+
+    void PlayRoomText(Room room)
+    {
+        text.text = room.roomName;
+        animator.Play("RoomPopup", 0, 0f);
+    }
+}
