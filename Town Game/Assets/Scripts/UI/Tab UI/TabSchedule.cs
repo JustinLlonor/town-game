7de20cb388bf931f83c6bd096e7e96c50f85b54e
JustinLn <102838453+Glorious-Toast@users.noreply.github.com>
2024-05-09 22:17:26 -0600

    Tab Schedule Start

---
+++ Town Game/Assets/Scripts/UI/Tab UI/TabSchedule.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TabSchedule : MonoBehaviour
+{
+    public Transform blockHolder;
+    public GameObject blockPrefab;
+    public TextMeshProUGUI dateText;
+    public int readDay;
+
+    // Shows the schedule a day in advance
+    public void ScrollForward()
+    {
+
+    }
+
+    // Shows the schedule a day before
+    public void ScrollBackward()
+    {
+
+    }
+}
