9d0f7f80f29350ad8f019ba10d426f7040483311
Glorious-Toast <102838453+Glorious-Toast@users.noreply.github.com>
2024-10-02 14:04:53 -0600

    Fix Render Texture Bug

--- Town Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Town Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.InputSystem;
-using JetBrains.Annotations;
 
 public class PlayerMovement : MonoBehaviourPunCallbacks
 {
