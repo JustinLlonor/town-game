766ad4f009f86962a20ab03832990788790bd35c
Justin LLONOR <102838453+Glorious-Toast@users.noreply.github.com>
2025-03-18 15:20:15 -0600

    Add Glitch Initiation Animation

--- Town Game/Assets/Scripts/UI/Glitch Effect/UIGlitch.cs
+++ Town Game/Assets/Scripts/UI/Glitch Effect/UIGlitch.cs
@@ -9,12 +9,17 @@ public class UIGlitch : MonoBehaviour
     public Transform renderTextureHolder;
     public float textureOffsetFrequency = 5f; // Times per second a texture gets offset
     public float maxTextureOffsetDistance = 10f; // Maximum offset
+    public float maxTextOffsetDistance = 60f; // Maximum offset
+    public AnimationCurve textOffsetCurve;
+    public float textureOffsetStartLength = .8f; // Length of enable animation
+    public float textureOffsetStart = 500f; // When enabling
     float offsetTimer;
     int renderTextureIndex;
+    float currentTextureOffset;
+    float textureOffsetTime;
     [Header("Text Effect Settings")]
     public Transform textHolder;
     public float textOffsetFrequency = 4f; // Times per second a text gets offset
-    public float maxTextOffsetDistance = 60f; // Maximum offset
     public string text = "ATTACK";
     float textOffsetTimer;
     int textIndex = 0;
@@ -25,12 +30,28 @@ public class UIGlitch : MonoBehaviour
         {
             child.GetComponent<TextMeshProUGUI>().text = text;
         }
+        currentTextureOffset = textureOffsetStart;
+        textureOffsetTime = 0f;
     }
 
     private void Update()
     {
+        ChangeTextOffset();
         TextureOffset();
         TextOffset();
+    }
+
+    void ChangeTextOffset()
+    {
+        if (textureOffsetTime > textureOffsetStartLength) return;
+        textureOffsetTime += Time.deltaTime;
+        if (textureOffsetTime > textureOffsetStartLength)
+        {
+            currentTextureOffset = maxTextureOffsetDistance;
+            return;
+        }
+        float progress = textureOffsetTime / textureOffsetStartLength;
+        currentTextureOffset = Mathf.Lerp(textureOffsetStart, maxTextureOffsetDistance, textOffsetCurve.Evaluate(progress));
     }
 
     void TextureOffset()
@@ -39,7 +60,7 @@ public class UIGlitch : MonoBehaviour
         if (offsetTimer < 1f) return;
         offsetTimer = 0f;
         Transform childTransform = renderTextureHolder.GetChild(renderTextureIndex);
-        Vector2 offset = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * maxTextureOffsetDistance;
+        Vector2 offset = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * currentTextureOffset;
         childTransform.localPosition = offset;
         renderTextureIndex++;
         if (renderTextureIndex >= renderTextureHolder.childCount)
