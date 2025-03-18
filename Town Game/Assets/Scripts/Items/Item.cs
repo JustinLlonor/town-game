107914f0c3b34c82b2ea2c342f1a3261b0e2f513
Justin LLONOR <102838453+Glorious-Toast@users.noreply.github.com>
2025-03-18 14:55:51 -0600

    Glitch Effect Tweaks

--- Town Game/Assets/Scripts/Items/Item.cs
+++ Town Game/Assets/Scripts/Items/Item.cs
@@ -21,10 +21,10 @@ public class Item : ScriptableObject
     [Header("Client Animation")]
     public string holdPose = "ArmHoldNormal_f";
     public string gripPose;
-    [Tooltip("The client sided use animations for this item. For weapons, the first state is attack, and the second state is defense.")]
+    [Tooltip("The client sided use animations for this item. For weapons, index 0 is attack, index 1 is defense, and index 2 is engagement")]
     public string[] clientAnimations;
     [Header("Character Animation")]
-    [Tooltip("The server sided use animations for this item. For weapons, the first state is attack, and the second state is defense.")]
+    [Tooltip("The server sided use animations for this item. For weapons, index 0 is attack, index 1 is defense, and index 2 is engagement")]
     public AnimationState[] useAnimations;
     public AnimationState[] holdPoses;
     
