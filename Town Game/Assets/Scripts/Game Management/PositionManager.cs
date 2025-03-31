bd23a821f8763d4d6df69b66f8624edd84ecf9e5
Glorious-Toast <gloriouslytoasted@gmail.com>
2025-03-31 03:29:02 -0600

    Add Voting Manager and Position Manager

---
+++ Town Game/Assets/Scripts/Game Management/PositionManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class PositionManager : NetworkBehaviour
+{
+    public struct Branch
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+}
