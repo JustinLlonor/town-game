d9ff60737f492f0317bce5481e49d4526e79c299
Glorious-Toast <gloriouslytoasted@gmail.com>
2025-05-08 21:27:34 -0600

    Add JobDescUI base

---
+++ Town Game/Assets/Scripts/UI/Tab UI/JobDescUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class JobDescUI : MonoBehaviour
+{
+    public TextMeshProUGUI jobTitle;
+    public TextMeshProUGUI jobPlayerCount;
+    public TextMeshProUGUI jobDescription;
+    public TextMeshProUGUI deadlineText;
+    public TextMeshProUGUI accessText;
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI buttonText;
+}
