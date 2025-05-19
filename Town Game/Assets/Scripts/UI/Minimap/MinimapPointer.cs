9a70b60fc0665fd414ecc1cb3d233105c402a500
Glorious-Toast <gloriouslytoasted@gmail.com>
2025-05-19 10:59:33 -0600

    Add constructor

--- Town Game/Assets/Scripts/UI/Minimap/MinimapPointer.cs
+++ Town Game/Assets/Scripts/UI/Minimap/MinimapPointer.cs
@@ -9,4 +9,12 @@ public class MinimapPointer
     /// If this pointer disappears when it gets within radius
     /// </summary>
     public bool disappearOnSight = false;
+
+    public MinimapPointer(string name, Vector3 position, Color color, bool disappearOnSight)
+    {
+        this.name = name;
+        this.position = position;
+        this.color = color;
+        this.disappearOnSight = disappearOnSight;
+    }
 }
