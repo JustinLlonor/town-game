310631cda6296a527fcaea0582664b31c7ca38b9
Glorious-Toast <gloriouslytoasted@gmail.com>
2025-07-05 12:39:12 -0600

    Base node stuff

---
+++ Town Game/Assets/Scripts/UI/Player Menu/Flowchart UI/FlowchartUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowchartUI : MonoBehaviour
+{
+    public Vector2Int gridSize;
+    public Vector2 cellSize;
+    public GameObject nodePrefab;
+    public GameObject arrowPrefab;
+}
